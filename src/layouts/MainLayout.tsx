bf33b14eadd7d8c0f918dc51af9675e6804596fd
DorisLupulescu <doris.lupulescu@gmail.com>
2024-03-28 19:51:26 +0200

    add main wrapper

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,18 @@
+import { styled } from '@mui/material'
 import { Outlet } from 'react-router-dom'
 import { Header } from '../components'
+
+const MainWrapper = styled('main')`
+    height: 100vh;
+`
 
 const MainLayout = () => {
     return (
         <>
             <Header />
-            <Outlet />
+            <MainWrapper>
+                <Outlet />
+            </MainWrapper>
         </>
     )
 }
