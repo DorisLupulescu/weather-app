5c3195ee340c8f59f258b9a8b74f7a74d02bf867
DorisLupulescu <doris.lupulescu@gmail.com>
2024-05-08 14:59:22 +0300

    add weather slice to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import TodayWeatherReducer from './slices/todayWeather'
 import UserSettingsReducer from './slices/userSettings'
 
 export const store = configureStore({
     reducer: {
         settings: UserSettingsReducer,
+        weather: TodayWeatherReducer,
     },
 })
 
